768854bee3248e9a2a12541762ff5f30382a129b
satheshkumardsk <satheshkumardsk@gmail.com>
2021-06-30 02:02:57 +0530

    first commit

---
+++ database/migrations/2021_06_29_193849_create_employees_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+
+            // $table->unsignedBigInteger('company_id');
+            // $table->foreign('company_id')->references('id')->on('companies');
+            $table->foreignId('company_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+
+            $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->string('designation');
+            $table->integer('active_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
